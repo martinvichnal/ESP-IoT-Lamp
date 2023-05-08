77e185062ac9dd90437d02e82939d1ab8252d3c3
vmazser <vmazsit@gmail.com>
2023-05-08 21:28:04 +0200

    JSON comm done

--- data/index.js
+++ data/index.js
@@ -1,6 +1,25 @@
 var gateway = `ws://${window.location.hostname}/ws`;
 var websocket;
 
+// var state = false;
+// var mode = 0;
+// var brightnessValue = 0;
+// var speedValue = 0;
+// var redValue = 0;
+// var greenValue = 0;
+// var blueValue = 0;
+
+
+const obj = {
+    state: 0,
+    mode: 0,
+    brightnessValue: 0,
+    speedValue: 0,
+    redValue: 0,
+    greenValue: 0,
+    blueValue: 0,
+}
+
 // ----------------------------------------------------------------------------
 // Initialization
 // ----------------------------------------------------------------------------
@@ -9,6 +28,7 @@ window.addEventListener('load', onLoad);
 
 function onLoad(event) {
     initWebSocket();
+    document.getElementById("sendBtn").addEventListener("click", btnPressed);
 }
 
 // ----------------------------------------------------------------------------
@@ -35,31 +55,62 @@ function onClose(event) {
 function onMessage(event) {
     let data = JSON.parse(event.data);
     console.log(data);
-    console.log(event);
-    document.getElementById('state').innerHTML = data.state;
+    // document.getElementById('state').innerHTML = data.state;
+    obj.state = data.state;
+    obj.mode = data.mode;
+    obj.brightnessValue = data.brightnessValue;
+    obj.speedValue = data.speedValue;
+    obj.redValue = data.redValue;
+    obj.greenValue = data.greenValue;
+    obj.blueValue = data.blueValue;
+
+    document.getElementById('state').innerHTML = obj.state;
+    document.getElementById('WSinfo1').innerHTML = obj.mode;
+    document.getElementById('WSinfo2').innerHTML = obj.brightnessValue;
+    document.getElementById('WSinfo3').innerHTML = obj.speedValue;
 }
 
-function toggle() {
-    console.log("Sending 'toggle' with websocket");
+function sendWebSocket(sendObj) {
     // websocket.send('toggle');
-    websocket.send(JSON.stringify({'action':'toggle'}));
+    // websocket.send(JSON.stringify({'action':'toggle'}));
+    console.log("SENDING THIS:");
+    console.log(sendObj);
+    websocket.send(JSON.stringify(sendObj));
 }
 
+function btnPressed()
+{
+    console.log("Send button was pressed. Sending websockets...");
+    sendWebSocket(obj);
+}
 
 
 function setRGB() {
-    var redValue = document.getElementById('red').value;
-    var greenValue = document.getElementById('green').value;
-    var blueValue = document.getElementById('blue').value;
-    var xhttp = new XMLHttpRequest();
-    xhttp.open('GET', '/setRGB?red=' + redValue + '&green=' + greenValue + '&blue=' + blueValue, true);
-    xhttp.send();
+    obj.redValue = document.getElementById('red').value;
+    obj.greenValue = document.getElementById('green').value;
+    obj.blueValue = document.getElementById('blue').value;
 }
 
 function buttonClick(state) {
-    // var btn1 = document.getElementById("btnOn");
-    // var btn2 = document.getElementById("btnOff");
-    var xhttp = new XMLHttpRequest();
-    xhttp.open('GET', '/setBtn?state=' + state, true);
-    xhttp.send();
+    obj.state = state;
 }
+
+
+
+
+// function setRGB() {
+//     var redValue = document.getElementById('red').value;
+//     var greenValue = document.getElementById('green').value;
+//     var blueValue = document.getElementById('blue').value;
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.open('GET', '/setRGB?red=' + redValue + '&green=' + greenValue + '&blue=' + blueValue, true);
+//     xhttp.send();
+// }
+
+// function buttonClick(state) {
+//     // var btn1 = document.getElementById("btnOn");
+//     // var btn2 = document.getElementById("btnOff");
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.open('GET', '/setBtn?state=' + state, true);
+//     xhttp.send();
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -9,74 +9,79 @@
 #include <ArduinoJson.h>
 #include "FileSystem.h"
 
+//==========================================================================================//
+//*******************************//
+// initialize fucntions
+//*******************************//
 void initWiFi();
 void initWebServer();
 void initWebSocket();
 
+//*******************************//
+// web page fucntions
+//*******************************//
 void handleRoot(AsyncWebServerRequest *request);
 void handleToggleLed(AsyncWebServerRequest *request);
 void handleSetRGB(AsyncWebServerRequest *request);
 void handleBtnState(AsyncWebServerRequest *request);
 
-void onEvent(AsyncWebSocket *server,       //
-             AsyncWebSocketClient *client, //
-             AwsEventType type,            // the signature of this function is defined
-             void *arg,                    // by the `AwsEventHandler` interface
-             uint8_t *data,                //
+//*******************************//
+// WebSocket fucntions
+//*******************************//
+void onEvent(AsyncWebSocket *server,
+             AsyncWebSocketClient *client,
+             AwsEventType type,
+             void *arg,
+             uint8_t *data,
              size_t len);
 void handleWebSocketMessage(void *arg, uint8_t *data, size_t len);
 void notifyClients();
 
+//*******************************//
+// Multi Core fucntions
+//*******************************//
 TaskHandle_t Core0; // will be used for wifi
 void initCores();
 void core0Func(void *);
 
-#define HTTP_PORT 80
 
-// Create an instance of the web server
+//*******************************//
+// WiFi
+//*******************************//
+#define HTTP_PORT 80
 AsyncWebServer server(HTTP_PORT);
 AsyncWebSocket ws("/ws");
 
 // Replace with your network credentials
-const char *ssid = "//";
-const char *password = "//";
+const char *ssid = "";
+const char *password = "";
 
-// Define pins for the LEDs
-const int ledPin = 2;
-const int LED1_PIN = 12;
-const int LED2_PIN = 14;
-const int LED3_PIN = 27;
-const int RED_PIN = 26;
-const int GREEN_PIN = 25;
-const int BLUE_PIN = 33;
-
-// Define variables for LED states
-bool led1State = false;
-bool led2State = false;
-bool led3State = false;
+//*******************************//
+// Variables
+//*******************************//
+bool state = false;
+int mode = 0;
+int brightnessValue = 0;
+int speedValue = 0;
 int redValue = 0;
 int greenValue = 0;
 int blueValue = 0;
+//******************************************************************************************//
+//==========================================================================================//
 
 
 //==========================================================================================//
-//           _____    ______  ______   __  __    ____
-//          / ___/   / ____/ /_  __/  / / / /   / __ \
-//          \__ \   / __/     / /    / / / /   / /_/ /
-//         ___/ /  / /___    / /    / /_/ /   / ____/
-//        /____/  /_____/   /_/     \____/   /_/
+//    _____ ______________  ______ 
+//   / ___// ____/_  __/ / / / __ \
+//   \__ \/ __/   / / / / / / /_/ /
+//  ___/ / /___  / / / /_/ / ____/ 
+// /____/_____/ /_/  \____/_/      
 //==========================================================================================//
 void setup()
 {
   Serial.begin(115200);
   //    *!! when core 0 is running empty it crashes every time...
   initCores();
-  pinMode(LED1_PIN, OUTPUT);
-  pinMode(LED2_PIN, OUTPUT);
-  pinMode(LED3_PIN, OUTPUT);
-  pinMode(RED_PIN, OUTPUT);
-  pinMode(GREEN_PIN, OUTPUT);
-  pinMode(BLUE_PIN, OUTPUT);
 }
 //******************************************************************************************//
 //==========================================================================================//
@@ -88,31 +93,21 @@ void setup()
 //  / /___/ /_/ / / /_/ /  / ____/
 // /_____/\____/  \____/  /_/
 //==========================================================================================//
-
-int i = 1;
-
 void loop()
 {
-  if (i > 2400)
-  {
-    i = 1;
-  }
-  else
-  {
-    redValue = i;
-    greenValue = i * 2;
-    blueValue = i * i;
-  }
-  i++;
+
 }
 //******************************************************************************************//
 //==========================================================================================//
 
+
+
 //==========================================================================================//
-//   _____  ____    ___    ____       ___         __   ____   ____    ___
-//  / ___/ / __ \  / _ \  / __/ ____ / _ \       / /  / __ \ / __ \  / _ \
-// / /__  / /_/ / / , _/ / _/  /___// // /      / /__/ /_/ // /_/ / / ___/
-// \___/  \____/ /_/|_| /___/       \___/      /____/\____/ \____/ /_/
+//    __________  ____  ______       __    ____  ____  ____ 
+//   / ____/ __ \/ __ \/ ____/      / /   / __ \/ __ \/ __ \
+//  / /   / / / / /_/ / __/        / /   / / / / / / / /_/ /
+// / /___/ /_/ / _, _/ /___       / /___/ /_/ / /_/ / ____/ 
+// \____/\____/_/ |_/_____/      /_____/\____/\____/_/      
 //==========================================================================================//
 int dummy = 0;
 void core0Func(void *)
@@ -126,12 +121,35 @@ void core0Func(void *)
   while (true)
   {
     ws.cleanupClients();
-    delay(50);
+    // delay(50);
+    // mode = dummy;
+    // brightnessValue = dummy + dummy;
+    // speedValue = dummy * 2;
+    // dummy++;
+    delay(1000);
+    // notifyClients();
+
+    // Serial.print("state:    "); Serial.println(state);
+    // Serial.print("mode:   "); Serial.println(mode);
+    // Serial.print("brightnessValue:    "); Serial.println(brightnessValue);
+    // Serial.print("speedValue:   "); Serial.println(speedValue);
+    // Serial.print("redValue:   "); Serial.println(redValue);
+    // Serial.print("greenValue:   "); Serial.println(greenValue);
+    // Serial.print("blueValue:    "); Serial.println(blueValue);
+    // Serial.println();
   }
 }
 //******************************************************************************************//
 //==========================================================================================//
 
+
+//==========================================================================================//
+//     ________  ___   ______________________  _   _______
+//    / ____/ / / / | / / ____/_  __/  _/ __ \/ | / / ___/
+//   / /_  / / / /  |/ / /     / /  / // / / /  |/ /\__ \ 
+//  / __/ / /_/ / /|  / /___  / / _/ // /_/ / /|  /___/ / 
+// /_/    \____/_/ |_/\____/ /_/ /___/\____/_/ |_//____/  
+//==========================================================================================//
 void initWiFi()
 {
   // Connecting to WiFi and setting local ip
@@ -154,8 +172,6 @@ void initWebServer()
   //    * "/" root page - index.html (from SPIFFS)
   //    * "/setRGB" requesting RGB walues from sliders - connected with handleSetRGB()
   //    * "/setBtn" requesting Button state values (0, 1, 2, 3) - connected with handleBtnState()
-  //    * "/setRGB" requesting RGB walues from sliders - connected with handleSetRGB()
-  //    * "/readings" requesting JSON object
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
             { request->send(SPIFFS, "/index.html", "text/html"); });
@@ -203,33 +219,58 @@ void handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
   AwsFrameInfo *info = (AwsFrameInfo *)arg;
   if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT)
   {
-    const uint8_t size = JSON_OBJECT_SIZE(1);
+    const uint8_t size = JSON_OBJECT_SIZE(7);
     StaticJsonDocument<size> json;
     DeserializationError err = deserializeJson(json, data);
+    // ERROR
     if (err)
     {
       Serial.print(F("deserializeJson() failed with code "));
       Serial.println(err.c_str());
       return;
     }
-    
-    const char *action= json["action"];
-    if (strcmp(action, "toggle") == 0)
-    {
-      led3State = !led3State;
-      Serial.println(led3State);
-      notifyClients();
-    }
+
+    state = json["state"];
+    mode = json["mode"];
+    brightnessValue = json["brightnessValue"];
+    speedValue = json["speedValue"];
+    redValue = json["redValue"];
+    greenValue = json["greenValue"];
+    blueValue = json["blueValue"];
+
+    // int _redValue = json["redValue"];
+    Serial.println(redValue);
+
+    // const char *_state = json["state"];
+    // Serial.println(_state);
+    // if (strcmp(_state, "1") == 0)
+    // {
+    //   state = true;
+    //   Serial.println(state);
+    // }
+    // if (strcmp(_state, "0") == 0)
+    // {
+    //   state = false;
+    //   Serial.println(state);
+    // }
   }
+  notifyClients();
 }
 
 void notifyClients()
 {
-  const uint8_t size = JSON_OBJECT_SIZE(1);
+  const uint8_t size = JSON_OBJECT_SIZE(7);
   StaticJsonDocument<size> json;
-  json["state"] = led3State;
+  json["state"] = state;
+  json["mode"] = mode;
+  json["brightnessValue"] = brightnessValue;
+  json["speedValue"] = speedValue;
+  json["redValue"] = redValue;
+  json["greenValue"] = greenValue;
+  json["blueValue"] = blueValue;
 
-  char data[17];
+  // char data[17];
+  char data[size];
   size_t len = serializeJson(json, data);
   ws.textAll(data, len);
 }
@@ -237,32 +278,32 @@ void notifyClients()
 // Handler function for setting the RGB color
 void handleSetRGB(AsyncWebServerRequest *request)
 {
-  String redStr = request->getParam("red")->value();
-  String greenStr = request->getParam("green")->value();
-  String blueStr = request->getParam("blue")->value();
-  redValue = redStr.toInt();
-  greenValue = greenStr.toInt();
-  blueValue = blueStr.toInt();
-  analogWrite(RED_PIN, redValue);
-  analogWrite(GREEN_PIN, greenValue);
-  analogWrite(BLUE_PIN, blueValue);
-  Serial.print("Set RGB: (");
-  Serial.print(redValue);
-  Serial.print(", ");
-  Serial.print(greenValue);
-  Serial.print(", ");
-  Serial.print(blueValue);
-  Serial.println(")");
-  request->send(200);
+  // String redStr = request->getParam("red")->value();
+  // String greenStr = request->getParam("green")->value();
+  // String blueStr = request->getParam("blue")->value();
+  // redValue = redStr.toInt();
+  // greenValue = greenStr.toInt();
+  // blueValue = blueStr.toInt();
+  // analogWrite(RED_PIN, redValue);
+  // analogWrite(GREEN_PIN, greenValue);
+  // analogWrite(BLUE_PIN, blueValue);
+  // Serial.print("Set RGB: (");
+  // Serial.print(redValue);
+  // Serial.print(", ");
+  // Serial.print(greenValue);
+  // Serial.print(", ");
+  // Serial.print(blueValue);
+  // Serial.println(")");
+  // request->send(200);
 }
 
 void handleBtnState(AsyncWebServerRequest *request)
 {
-  String btnStr = request->getParam("state")->value();
-  int btnState = btnStr.toInt();
+  // String btnStr = request->getParam("state")->value();
+  // int btnState = btnStr.toInt();
 
-  Serial.println(btnState);
-  request->send(200);
+  // Serial.println(btnState);
+  // request->send(200);
 }
 
 void initCores()
